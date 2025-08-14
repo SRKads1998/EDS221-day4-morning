f0f6883ec98c7acdb28f206f49fa3e2006cee93a
SRKads1998 <kadonoffs@gmail.com>
2025-08-14 11:58:20 -0700

    functions

---
+++ function practice.R
@@ -0,0 +1,61 @@
+# adds up the number of birds and dogs
+
+#defined function
+birddog_sum <- function(bird, dog) {
+  pets = bird + dog
+  return(pets)
+}
+
+# use function
+total_pets <- birddog_sum(bird = 2, dog = 5)
+
+
+total_pets <- birddog_sum(2, 5)
+
+# create function to double values
+
+
+double_it <- function(x) {
+  print(2 * x)
+}
+
+double_it(24)
+
+double_it(c(1, 0.5, -200))
+
+quarter_splits <- c(1, 1.1, 1.2, 1.1, 1.4, 1.5, 1.6, 1.4)
+
+for (i in 1:length(quarter_splits)) {
+  quarter_splits[i] + quarter_splits[i+1]
+}
+
+# function w/ conditionals
+# example is converting animals ages
+
+animal_age <- function(animal, age)
+{ if (animal == "dog") {
+  print(age * 7)
+} else if (animal == "goat") {
+  print (age * 4.7)
+}
+}
+# 8 year pooch
+animal_age(animal = "dog", age = 8)
+
+# try this w/ a cow
+animal_age(animal = "cow", age = 8)
+
+# write an updated version of code w/ error
+
+animal_age_stop <- function(animal, age){
+  if (animal %in% c("dog", "goat")) {
+    stop("Animal must be dog or goat") }
+  if (is.numeric(age) = FALSE) {
+    stop("The age must be a number")
+  }
+  if (age <= 0 | age > 50) {
+    warning("are you sure about age?")
+  }
+}
+
+
